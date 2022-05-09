d61052c341e804e5e2699adc9e3bb43fbfa90728
sougabioliveira <oliveira.gabrielagomes@gmail.com>
2022-05-09 12:53:08 -0300

    Adicionando ajustes

--- src/main/java/com/generation/lojadegames/repository/ProdutoRepository.java
+++ src/main/java/com/generation/lojadegames/repository/ProdutoRepository.java
@@ -3,10 +3,11 @@ package com.generation.lojadegames.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import com.generation.lojadegames.model.ProdutoModel;
 
+@Repository
 public interface ProdutoRepository extends JpaRepository<ProdutoModel, Long> {
-	public List<ProdutoModel> findAllByTituloContainingIgnoreCase(@Param("titulo") String titulo);
+	public List<ProdutoModel> findAllByTituloContainingIgnoreCase(String titulo);
 }
