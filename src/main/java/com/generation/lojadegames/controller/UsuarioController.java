b343067d962ab20fd6f7301a96b7a6f645ccbb4e
sougabioliveira <oliveira.gabrielagomes@gmail.com>
2022-05-11 21:34:45 -0300

    Adicionando service e security

--- src/main/java/com/generation/lojadegames/controller/UsuarioController.java
+++ src/main/java/com/generation/lojadegames/controller/UsuarioController.java
@@ -1,5 +1,6 @@
 package com.generation.lojadegames.controller;
 
+
 import java.util.List;
 import java.util.Optional;
 
@@ -9,8 +10,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -55,4 +59,14 @@ public class UsuarioController {
 					.orElse(ResponseEntity.status(HttpStatus.BAD_REQUEST).build());
 
 		}
+		
+		 @PutMapping
+		 public ResponseEntity<UsuarioModel> put (@Valid @RequestBody UsuarioModel usuario) {
+			 return ResponseEntity.status(HttpStatus.OK).body(usuarioRepository.save(usuario));
+		 }
+		 
+		 @DeleteMapping("/{id}")
+		 public void delete(@PathVariable long id) {
+			usuarioRepository.deleteById(id); 
+		 }
 	}
